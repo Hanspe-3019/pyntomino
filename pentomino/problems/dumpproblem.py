718745c3342070f436369c6248dc3abcc6530399
Hp i <sql.904@me.com>
2024-04-18 11:31:54 +0200

    refactored: pentomino/problems/dumpproblem.py

--- pentomino/problems/dumpproblem.py
+++ pentomino/problems/dumpproblem.py
@@ -1,19 +1,10 @@
 ''' Modul mit dem ein Problem-Raum in python code verwandelt wird: 
 '''
-import numpy as np
-def do(problem):
-    ''' problem ist ein np.ndarray mit ndim=3
+def source(problem):
+    ''' -
     '''
-    print(problem.shape)
-    code = [ f'    raum = init(({problem.shape})', ]
-
-    notempty_at = np.array(np.where(problem>=0)).T
-    for at in notempty_at:
-        # at ist ein ndarray, ndim=1, len=3, als String '[x y z]'
-        lhs = f'raum{str(at).replace(" ", ",")} = '
-        rhs = f'{problem[at[0], at[1], at[2]]}'
-        code.append(lhs + rhs)
-
-
-    code.append('return raum')
-    print('\n    '.join(code))
+    print('def problem_<name>():')
+    print('    " - "')
+    code = repr(problem).split('\n')
+    print(f'    return np.{code[0]}')
+    print('    ' + '\n    '.join(code[1:]))
